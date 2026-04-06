6ac01683465e962e5771abb0b3e369f0d2ec4238
Terry Brown <brown.terryn@epa.gov>
2026-04-06 08:50:16 -0500

    Initial plugin work

--- src/pt/core.py
+++ src/pt/core.py
@@ -1,4 +1,9 @@
-"""Contract for a plugin."""
+"""Contract for a plugin, main app."""
+
+from textual.app import App, ComposeResult
+from textual.widgets import Footer, Header
+
+from pt.flexbox import FlexBoxContainer
 
 
 class Plugin:
@@ -11,3 +16,33 @@ class Plugin:
     ):
         self.name = name
         self.every = every
+
+class TextDeskLayout(FlexBoxContainer):
+    def compose(self) -> ComposeResult:
+        """Create child widgets for the app."""
+        for widget in self.app.plugins:
+            yield widget()
+
+class TextDeskApp(App):
+    BINDINGS = [("d", "toggle_dark", "Toggle dark mode")]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.plugins = []
+
+    def compose(self) -> ComposeResult:
+        """Create child widgets for the app."""
+        yield Header()
+        yield Footer()
+        yield TextDeskLayout()
+
+    def action_toggle_dark(self) -> None:
+        """An action to toggle dark mode."""
+        self.theme = (
+            "textual-dark" if self.theme == "textual-light" else "textual-light"
+        )
+
+
+if __name__ == "__main__":
+    app = TextDeskApp()
+    app.run()
